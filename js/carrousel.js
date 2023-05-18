71ecb8bd7d23aac209220347b6bcf4ab8c62fe8d
Vanegas Bustamante <e1872649@cmaisonneuve.qc.ca>
2023-05-18 13:39:45 -0400

    s16 c1 Changement de position des boutons du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,140 +1,181 @@
 (function(){
-    console.log('Début du carrousel')
-   let bouton = document.querySelector(".carrousel__ouvrir")
-   /* -------------------------------------------------------- Variable du carrousel */
-   let carrousel  = document.querySelector(".carrousel")
-   let carrousel__x = document.querySelector(".carrousel__x")
-   let carrousel__figure = document.querySelector(".carrousel__figure")
-   let carrousel__form = document.querySelector(".carrousel__form")
-   console.log(carrousel__form.tagName)
-   /* -------------------------------------------------------- Variable de la galerie */
-   let galerie = document.querySelector(".galerie")
-   let galerie__img = galerie.querySelectorAll("img")
-   // console.log("galerie__img: " + galerie__img.length)
-   // console.log(carrousel.tagName)
-   /* ------------------------------------------ positionnement de l'image active du carrousel */
-   let index = 0
-   let ancien_index = -1
-   let position = 0 // permet d'indexer les image de la galerie et 
-   
-   /* ----------------------------------------------------  ouvrir boîte modale */
-       ajouter_img_dans_carrousel()
-
-   bouton.addEventListener('mousedown', function(){
-       console.log('ouvrir la boîte modale')
-       carrousel.classList.add('carrousel--activer')
-
-   
-   })
-   /* ----------------------------------------------------  fermer boîte modale */
-   carrousel__x.addEventListener('mousedown', function(){
-       console.log('fermer la boîte modale')
-       carrousel.classList.remove('carrousel--activer')
-   
-   })
-
-   galerie.addEventListener('mousedown', function(){
-    console.log('ouvrir la boîte modale')
-   
-    if( !carrousel.classList.contains('carrousel--activer')){
-         carrousel.classList.add('carrousel--activer')
-   
-    }
+  console.log('Début du carrousel')
+ /* -------------------------------------------------------- Variable du carrousel */
+ let carrousel  = document.querySelector(".carrousel")
+ let carrousel__x = document.querySelector(".carrousel__x")
+ let carrousel__figure = document.querySelector(".carrousel__figure")
+ let carrousel__form = document.querySelector(".carrousel__form")
+ let carrousel__gauche = document.querySelector(".carrousel__gauche")
+ console.log("carrousel__gauche= "+carrousel__gauche.tagName)
+ let carrousel__droit = document.querySelector(".carrousel__droit")
+ console.log(carrousel__form.tagName)
+ /* -------------------------------------------------------- Variable de la galerie */
+ let galerie = document.querySelector(".galerie")
+ let galerie__img = galerie.querySelectorAll("img")
+ // console.log("galerie__img: " + galerie__img.length)
+ // console.log(carrousel.tagName)
+ /* ------------------------------------------ positionnement de l'image active du carrousel */
+ let index = 0 // permet d'identifier l'image courante du carrousel
+ let ancien_index = -1 // permet d'identidier l'image précédente
+ let position = 0 // permet d'indexer les image de la galerie et 
  
-    
-   
-    //ajouter_img_dans_carrousel()
-
-})
-   
-   
-   
-   /** 
-    * ajouter_img_dans_carrousel
-    * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
-    */
-   function ajouter_img_dans_carrousel()
+ /**
+  * Permet de créer le carrousel en parcourant la galerie d'image
+  */
+ ajouter_img_dans_carrousel()
+ 
+ /**
+  * navigation dans le carrousel avec le bouton précédent: carrousel__precedent
+  */
+ carrousel__gauche.addEventListener('mousedown', function(){
+  index = index-1
+  if (index==-1){
+   index = galerie__img.length-1
+  }
+  console.log(index)
+  afficher_image(index)
+ 
+ })
+ /**
+  * navigation dans le carrousel avec le bouton suivant: carrousel__suivant
+  */
+ carrousel__droit.addEventListener('mousedown', function(){
+   index =  parseInt(index)+1
+   if(index==galerie__img.length)
    {
-     
-     for (const elm of galerie__img)
-     {
-       elm.dataset.index = position
-       elm.addEventListener('mousedown',function(){
-         index = this.dataset.index
-         afficher_image(index)
-         console.log(index)
-       })
-   
-       creation_img_carrousel(elm)
-       creation_radio_carrousel()
-       bouton_image()
-     }
+     index = 0;
    }
+   console.log(index)
+   afficher_image(index)
+  
+  })
+ 
+ /* ----------------------------------------------------  fermer boîte modale */
+ carrousel__x.addEventListener('mousedown', function(){
+     console.log('fermer la boîte modale')
+     carrousel.classList.remove('carrousel--activer')
+ 
+ })
+ /** 
+  * ajouter_img_dans_carrousel
+  * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+  */
+ function ajouter_img_dans_carrousel()
+ {
    
-   function creation_img_carrousel(elm){
-         //console.log(elm.getAttribute('src'))
-         let img = document.createElement('img')
-         // img.setAttribute('src', elm.getAttribute('src'))
-          img.src = elm.src
-          img.classList.add('carrousel__img')
-          //console.log (img.getAttribute('src'))
-          carrousel__figure.appendChild(img)
-   }
-
-
-     /**
-    * Création des boutons droit et gauche
-    */
-
-     function bouton_image(){
-    
-            let btn = document.querySelector('carrousel__gauche')
-            let btn_2 = document.querySelector('carrousel__droit')
-      
-    
-     }
-    
-
-
-   /**
-    * Création d'un btnio-bouton
-    */
-   
-   
-   function  creation_radio_carrousel(){
-     let rad = document.createElement('input')
-     console.log(rad.tagName)
-     rad.setAttribute('type','radio')
-     rad.setAttribute('name', 'carrousel__rad')
-     rad.classList.add('carrousel__rad')
-     rad.dataset.index = position
-     position = position + 1 // incrémentation de 1
-     // position += 1
-     // position++
-     carrousel__form.appendChild(rad)
-     rad.addEventListener('mousedown', function(){
-       console.log(this.dataset.index)
-       index = this.dataset.index
+   for (const elm of galerie__img)
+   {
+     elm.dataset.index = position
+     elm.addEventListener('mousedown',function(e){
+       index = e.target.dataset.index
        afficher_image(index)
+       if (carrousel.classList.contains('carrousel--activer') == false){
+       /* ouvrie la boîte modale */
+       carrousel.classList.add('carrousel--activer')
+       }
+       console.log(index)
      })
+ 
+     creation_img_carrousel(elm)
+     creation_radio_carrousel()
    }
-   
-   function afficher_image(index){
-   
-     if (ancien_index != -1){
-      // carrousel__figure.children[ancien_index].style.opacity = 0  
-      carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer')
-      carrousel__form.children[ancien_index].checked = true
-     }
-     // carrousel__figure.children[index].style.opacity = 1
-     carrousel__figure.children[index].classList.add('carrousel__img--activer')
-     ancien_index = index
+ }
+ 
+ 
+ 
+ 
+ function creation_img_carrousel(elm){
+       //console.log(elm.getAttribute('src'))
+       let img = document.createElement('img')
+       // img.setAttribute('src', elm.getAttribute('src'))
+       /* retire les 12 dernier caractères « -150x150.jpg »*/
+       let longueur = elm.src.length-12
+ 
+      // let extension  =  elm.src.substr(0,-4)
+       extension = elm.src.substr(elm.src.lastIndexOf('.'),4)
+       console.log("extension="+extension)
+        img.src = elm.src.substr(0,longueur) + extension
+        img.classList.add('carrousel__img')
+        //console.log (img.getAttribute('src'))
+        carrousel__figure.appendChild(img)
+ }
+ /**
+  * Création d'un radio-bouton
+  */
+ 
+ 
+ function  creation_radio_carrousel(){
+   let rad = document.createElement('input')
+   console.log(rad.tagName)
+   rad.setAttribute('type','radio')
+   rad.setAttribute('name', 'carrousel__rad')
+   rad.classList.add('carrousel__rad')
+   rad.dataset.index = position
+   position = position + 1 // incrémentation de 1
+   // position += 1
+   // position++
+   carrousel__form.appendChild(rad)
+   rad.addEventListener('mousedown', function(e){
+     console.log(e.target.dataset.index)
+     index = e.target.dataset.index
+     afficher_image(index)
+   })
+ }
+ 
+ function afficher_image(index){
+ 
+   if (ancien_index != -1){
+    // carrousel__figure.children[ancien_index].style.opacity = 0  
+    carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer')
+    carrousel__form.children[ancien_index].checked = false 
    }
-
-   /**
-    * Permet de vérifier si la classe de carrousel «carrousel--activer» se trouve dans la liste des classe carrousel
-    * classlist.classList.contain('carrousel--activer')
-    */
-   
-   
-   })()
+   // carrousel__figure.children[index].style.opacity = 1
+   redimensionner_carrousel()
+   carrousel__figure.children[index].classList.add('carrousel__img--activer')
+   carrousel__form.children[index].checked = true
+   ancien_index = index
+ }
+ 
+ function redimensionner_carrousel(){
+   /* récupérer les dimensions de l'image courante */
+   const imageWidth = carrousel__figure.children[index].naturalWidth
+   const imageHeight = carrousel__figure.children[index].naturalHeight
+   const windowWidth = window.innerWidth
+   const windowHeight = window.innerHeight
+ 
+   let carrouselHeight = windowHeight 
+   if (windowWidth > 1000)
+   {
+     carrouselHeight = windowHeight - windowHeight/3
+   }
+ 
+   let carrouselWidth = carrouselHeight * imageWidth/imageHeight
+ 
+   carrousel.style.width = `${carrouselWidth}px`
+   carrousel.style.height = `${carrouselHeight}px`
+   carrousel.style.top= `${(windowHeight-carrouselHeight)/2}px`
+   carrousel.style.left= `${(windowWidth-carrouselWidth)/2}px`
+ 
+ 
+ 
+ 
+   console.log(
+   `imageWidth= ${imageWidth}
+   imageHeight= ${imageHeight}
+   windowWidth= ${windowWidth}
+   windowHeight= ${windowHeight}
+   `)
+ }
+ 
+ 
+ /*
+ permet de vérifier si la classe « carrousel--activer » 
+ se trouve dans la liste des classe carrousel
+   carrousel.classList.contain('carrousel--activer')
+ 
+   mdn classList.contain()
+ 
+ */
+ 
+ 
+ })()
